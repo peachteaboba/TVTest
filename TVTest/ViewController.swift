93b9fdfa915e219b451b809368d57f599fc7e67b
Andy Feng <anndyfeng1@gmail.com>
2016-08-04 11:14:14 -0700

    commit 2

--- TVTest/ViewController.swift
+++ TVTest/ViewController.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    
+    var tasks = ["Exercise for 30 minutes", "Wireframe for some project", "Do laundry"]
+    @IBOutlet weak var insertTaskField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
 
+    @IBAction func insertButtonPressed(sender: UIButton) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +29,22 @@ class ViewController: UIViewController {
     }
 
 
+    // how many cells are we going to need?
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    // how should I create each cell?
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // dequeue the cell from our storyboard
+        let cell = tableView.dequeueReusableCellWithIdentifier("MyCell")!
+        // if the cell has a text label, set it to the model that is corresponding to the row in array
+        cell.textLabel?.text = tasks[indexPath.row]
+        // return cell so that Table View knows what to draw in each row
+        return cell
+    }
+    
+    
+    
+    
 }
 
